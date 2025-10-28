84d336e17ae7b848d06c6a118c09ec76a2d01eee
Yoonis-T <yoonis.abdulla@edu.prakticum.fi>
2025-10-28 15:25:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOutput_in_C_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Be användaren skriva sitt namn
+            Console.WriteLine("Please enter your name:");
+
+            // 2. Läs in namnet från konsolen
+            string userName = Console.ReadLine();
+
+            // 3. Skriv ut en hälsning med användarens namn
+            Console.WriteLine("Hello, " + userName + "! Nice to meet you!");
+        }
+    }
+}
